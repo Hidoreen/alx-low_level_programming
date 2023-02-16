b5d04ad5d2b7484129736faf926461b0213ecb62
Doreen <doreenwangeci38@gmail.com>
2023-02-16 03:01:24 -0800

    Modified

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,14 +2,15 @@
 
 /**
  * This is a funtion that prints out output using the puts command
- * The return value is 0
+ * Main function: prints the output of the puts command
+ * Return: value is 0
 */
 
 int main(void)
 {
-	char learnc[100] = "\"Programming is like building a multilingual puzzle";
+char learnc[100] = "\"Programming is like building a multilingual puzzle";
 
- puts(learnc);
+puts(learnc);
 
 return (0);
 
