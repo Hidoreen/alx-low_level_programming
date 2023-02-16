0f5a10e32af5cf9b566122260042977d96bbfcfc
Doreen <doreenwangeci38@gmail.com>
2023-02-16 02:11:03 -0800

    Corrections

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 int main(void)
 {
 
-char learnc[100] = "Programming is like building a multilingual puzzle\n";
+char learnc[100] = ""Programming is like building a multilingual puzzle\n";
 	puts(learnc);
 
 
