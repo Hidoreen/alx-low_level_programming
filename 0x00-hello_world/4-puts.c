2cfaacb3e8ce805c4afe2b7e2a823343d74c6b3f
Doreen <doreenwangeci38@gmail.com>
2023-02-16 04:51:49 -0800

    Crazy betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+
 /**
  * main - A program that prints aline using the puts function
  * return: 0 (success)
-*/
+ */
+
 int main(void)
 {
-char learnc[100] = "\"Programming is like building a multilingual puzzle";
-puts(learnc);
-return (0);
+	char learnc[100] = "\"Programming is like building a multilingual puzzle";
+	puts(learnc);/* puts function called*/
+	return (0);
 }
