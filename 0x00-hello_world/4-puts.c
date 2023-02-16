5f92b01b4eedafafb2afaacb9f84b1f2ef493aa6
Doreen <doreenwangeci38@gmail.com>
2023-02-16 02:35:11 -0800

    added betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
+/** 
+ * This is a function that prints out output using the puts command
+ * The return value is 0
+*/
 int main(void)
 {
 
-char learnc[100] = "\"Programming is like building a multilingual puzzle\n, Hey C";
+char learnc[100] = "\"Programming is like building a multilingual puzzle\n";
+
 	puts(learnc);
 
-
-return 0;
+return (0);
 }
