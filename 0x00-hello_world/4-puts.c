2b2f3e6ff1514e86097015d5ea99cb2ca9f779ce
Doreen <doreenwangeci38@gmail.com>
2023-02-16 05:08:10 -0800

    betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - A program that prints aline using the puts function
- * return: 0 (success)
+ * main - This prints aline using the puts function
+ * return: This returns a value of  0 (success)
  */
 
 int main(void)
 {
-	char learnc[100] = "\"Programming is like building a multilingual puzzle";
-	puts(learnc);/* puts function called*/
+	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,10 +3,10 @@
 /**
  * main - a program that prints a line using the printf function
  * Return : 0 (success)
- */
+  */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");	
 	return (0);	
 }
