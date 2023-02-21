c0d9ebc1f1e23ac7f1356a1885a7d37464c050cd
Doreen <doreenwangeci38@gmail.com>
2023-02-21 15:27:13 -0800

    betty style

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,9 @@
 #include "main.h"
 
-/**
- * main - Entry point
- * Return: Always 0 (Success)
-*/
+/* print_alphabet - prints lowercase alphabet */
 
-void print_alphabet(void)/* print_alphabet -> prints the lowercase alphabets*/
+/*
+ * print_alphabet - Funcction prints alphabets */ void print_alphabet(void)
 {
 	char c;
 
