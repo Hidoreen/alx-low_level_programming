a4531e67a85ea222db422f1cee3be3a0d9b56abf
Doreen <doreenwangeci38@gmail.com>
2023-02-27 11:22:40 -0800

    pointers

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,3 +6,24 @@
  * @str: input
  * return - always 0
  */
+
+void puts_half(char *str)
+{
+	int len = 0;
+
+	while (*(str + len) != '\0')
+	{
+		len++;
+	}
+	if (!(len % 2))
+		len /= 2;
+	else
+		len = (len + 1) / 2;
+
+	while (*(str + len) != '\0')
+	{
+		_putchar(*(str + len));
+		len++;
+	}
+	_putchar('\n');
+}
