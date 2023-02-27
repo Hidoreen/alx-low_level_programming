56228fc7380dbbc95ddcea6a1a5488e9e405aef3
Doreen <doreenwangeci38@gmail.com>
2023-02-27 03:55:19 -0800

    strings

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * puts2 - a function that prints every character of a string
+ * @str: input
+ * return: always 0
+ */
+
+void puts2(char *str)
+{
+	int = o;
+
+	for (; str[o] != '\0')
+	{
+		if ((o % 2) == 0)
+		{
+			_putchar (str[0]);
+		else
+			continue;
+		}
+		_putchar ('\n');
+	}
+}
