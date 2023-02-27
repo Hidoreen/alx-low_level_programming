7a367e5c7a1246034a7071c09a172db46446d170
Doreen <doreenwangeci38@gmail.com>
2023-02-27 10:31:36 -0800

    reverse

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,21 +6,3 @@
  * @str: input
  * return - always 0
  */
-
-void puts_half(char *str)
-{
-	int i;
-	for (i = 0; str[i] != '\0'; i++)
-
-		int i;
-
-			for (i = 0; str[i] != '\0'; i++)
-						;
-
-				i++;
-					for (i /= 2; str[i] != '\0'; i++)
-							{
-										_putchar(str[i]);
-											}
-						_putchar('\n');
-}
