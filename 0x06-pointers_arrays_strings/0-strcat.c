f0db1308a6aa0be940986ec42753f79e19097b5f
Doreen <doreenwangeci38@gmail.com>
2023-03-02 00:04:40 -0800

    strcat code

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,7 +3,7 @@
 
 /**
  * *_strcat - is a function that concatenates two trings
- * @dest - the destination string
+ * @dest: the destination string parameter
  * @src: the string to be concatenated
  * Description: Copy the string pointed to by pointer `src` to
  * the buffer pointed to by `dest`
@@ -12,17 +12,15 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int i = 0;
-	int length = 0;
+	int j = 0;
+	int i;
 
-	while (dest[i] != '\0')
-	{
-		length++;
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+	do {
+		*(dest + i + j) = src[j];
+		j++
 	}
-	for (i = 0; src[i] != '\0'; i++; lenght++)
-	{
-		dest[length] = src[i];
-	}
-	dest[length] = '\0';
-	return (dest);
+	while (*(src + j))
+		return (dest);
 }
