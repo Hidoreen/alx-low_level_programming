c8a2af99caebde8e4a491e360eeff0d1b3ca2d13
Doreen <doreenwangeci38@gmail.com>
2023-03-02 00:16:24 -0800

    strcat code

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,15 +12,21 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int j = 0;
+	int j;
 	int i;
 
-	for (i = 0; dest[i] != '\0'; i++)
-		;
-	do {
-		*(dest + i + j) = src[j];
-		j++
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
 	}
-	while (*(src + j))
-		return (dest);
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
 }
