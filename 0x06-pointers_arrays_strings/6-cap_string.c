6fe558e2f37ad64b8caf340762ba6d8c174ad106
Doreen <doreenwangeci38@gmail.com>
2023-03-02 02:53:02 -0800

    more strings

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * *cap_string - is afunction that capitalizes all letters of a string
+ * @a: a pointer
+ * Return:a pointer in the changed state
+ */
+
+char *cap_string(char *a)
+{
+	int index = 0;
+
+	while (a[index])
+	{
+		while (!(a[index] >= 'a' && a[index] <= 'z'))
+			index++;
+		if (a[index - 1]  == ' ' ||
+				a[index - 1] == '\t' ||
+				a[index - 1] == '\n' ||
+				a[index - 1] == ',' ||
+				a[index - 1] == ';' ||
+				a[index - 1] == '.' ||
+				a[index - 1] == '!' ||
+				a[index - 1] == '?' ||
+				a[index - 1] == '"' ||
+				a[index - 1] == '(' ||
+				a[index - 1] == ')' ||
+				a[index - 1] == '{' ||
+				a[index - 1] == '}' ||
+				index == 0)
+			a[index] == 32;
+	}
+	return (a);
+}
