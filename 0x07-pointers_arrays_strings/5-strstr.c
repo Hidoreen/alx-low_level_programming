e151290109acea46277ce6e9ddc3fc6b2b9a1080
Doreen <doreenwangeci38@gmail.com>
2023-03-06 09:09:58 -0800

    pointers

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * *_strstr - a function that locates a substring
+ * @haystack: a pointer
+ * @needle: a pointer
+ * Return: a pointer to the begging of a substring
+ * otherwise return NULL
+ */
+
+
