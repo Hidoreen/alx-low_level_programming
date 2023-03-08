5c2b1edcb125eba8180f7b67f0ad05cf0c3f6b95
Doreen <doreenwangeci38@gmail.com>
2023-03-08 09:57:04 -0800

    modified

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,1 +1,41 @@
+#include <stdio.h>
+#include "main.h"
 
+/**
+ * myHelp- a function that gets the squares
+ *description- my help helps get root
+ * @i: number to square
+ * @n: number to check square
+ * return: square root
+ */
+
+int myHelp(int i, int n)
+{
+	if (i * i != n)
+	{
+		int j;
+
+		if (i > n)
+		{
+			return (-1);
+		}
+		j = myHelp(i + 1, n);
+			return (j + i);
+	}
+	return (0);
+}
+
+/**
+ * _sqrt_recursion - a funtion that returns the natural square of a number
+ * @n: integer
+ * Return: square root
+ */
+
+int _sqrt_recursion(int n)
+{
+	int i = 0;
+
+	if (myHelp(i, n) == n && n != 1)
+		return (-1);
+	return (myHelp(i, n));
+				}
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * check_prime - function helper
+ * @n: number track for prime
+ * @num:possible divisor
+ */
+
+int check_prime(int n, int check)
+{
+	if (n > check)
+	{
+		if (n % check == 0)
+			return (0);
+		return (check_prime(n, check + 1));
+	}
+	return (1);
+}
+
+/**
+ * is_prime_number - a function that prints prime numbers
+ * @n: an integer
+ * Return: int
+ */
+
+int is_prime_number(int n)
+{
+	if (n == 0)
+		return (0);
+	if (n < 0)
+		return (0);
+	if (n == 1)
+		return (0);
+       return (is_prime(n, 2));
+}       
