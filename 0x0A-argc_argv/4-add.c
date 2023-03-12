a7f52ee2b443684712e066f269bdf7e62cce96ed
Doreen <doreenwangeci38@gmail.com>
2023-03-11 20:43:50 -0800

    modified

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <libmy.h>
 #include <ctype.h>
+
+/**
+ * main - adds positive numbers followed by a new line.
+ * @argv: arrays of arguments.
+ * @argc: number of arguments.
+ * Return: 0.
+ */
+
 
 int main(int argc, char *argv[])
 {
@@ -9,7 +16,7 @@ int main(int argc, char *argv[])
 
 	for (num = 1; num < argc; num++)
 	{
-		for (digit = 0; argv[num][digit] != '\0'; digit+=)
+		for (digit = 0; argv[num][digit] != '\0'; digit++)
 		{
 			if (!isdigit(argv[num][digit]))
 			{
