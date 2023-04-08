59c18d445d40b87bba393cfe39ae9f1acbb063a3
Hidoreen <doreenwangeci38@gmail.com>
2023-04-08 21:59:09 +0300

    set bit

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-/*8
+/**
  * set_bit - sets the value of a bit at index
  * @n: the input to be checked
  * @index: the position of a bit
@@ -10,11 +10,9 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned int d;
 
 	if(index > 63)
 		return (-1);
-	d = 1 <, index;
-	*n = (*n | d);
+	*n ^= (1 << index);
 	return (1);
 }
